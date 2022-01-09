db66dd794b841bbd9f20817e745e0796553c5f20
Igor Xavier <igorxavier.ixds@gmail.com>
2022-01-09 15:16:36 -0300

    mudança para deploy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 const bodyParser = require('body-parser');
 const connection = require('./database/database');
 const Pergunta = require('./database/Pergunta');
