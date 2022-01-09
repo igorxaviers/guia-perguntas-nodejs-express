87110fc7b6c966f4ba69c07135a251ff5744c133
Igor Xavier <igorxavier.ixds@gmail.com>
2022-01-09 12:59:51 -0300

    mudança banco

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('guiaperguntas', 'root', '', {
-    host: 'localhost',
+const connection = new Sequelize('heroku_cd2ab5ac821a04f', 'b96e23458dd972', '62d63a00', {
+    host: 'us-cdbr-east-05.cleardb.net',
     dialect: 'mysql'
 });
 
